2b08d970ccd403a921f3cbf650db78b454295865
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-03-03 22:18:42 -0800

    test added if it contains the data or not

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    public void testContainsValue(){
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(10);
+        root.left = new BinaryTreeNode<Integer>(5);
+        root.right = new BinaryTreeNode<Integer>(15);
 
+        boolean result = (BstSearch.contains(root,5));
+
+        assertTrue(result);
+    }
+
+     @Test
+    public void notContainsValue(){
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(10);
+        root.left = new BinaryTreeNode<Integer>(5);
+        root.right = new BinaryTreeNode<Integer>(15);
+
+        boolean result = (BstSearch.contains(root,8));
+
+        assertFalse(result);
+    }
 }
